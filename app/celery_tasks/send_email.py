7501acc2c345dcaa0ac36eb57478cc58079defa2
Ngo Van Danh <danhnv.bhsoft@gmail.com>
2024-01-11 09:54:16 +0700

    feat: refactor code

--- app/celery_tasks/send_email.py
+++ app/celery_tasks/send_email.py
@@ -1,5 +1,5 @@
 from celery_config import celery
-from app.services.email_service import send_mail
+from app.services.send_email import send_mail
 from app.database import connect
 from app.repository.user import user_repository
 from celery import group
@@ -12,6 +12,7 @@ mail_data = {
 @celery.task(ignore_result=True)
 def send_welcome_email_task(mail_to: str, mail_data: dict):
     try:
+        # Sending mail
         print("Mail is sending...")
         send_mail(mail_to, mail_data)
         print("Mail has been sent!!!")
@@ -24,13 +25,18 @@ def send_reminder_order_email_task():
         # Connecting db
         connect()
 
-        # Handle send email
+        # Get users who need to send reminder email
         non_buying_customers = user_repository.get_user_has_not_order()
         chunk_customers_data = []
+
+        # Number of mails sending in parallel
         chunk_size = 10
-        for i in range(0, len(non_buying_customers), 10):
+
+        # Chunk data
+        for i in range(0, len(non_buying_customers), chunk_size):
             chunk_customers_data.append(non_buying_customers[i:i + chunk_size])
 
+        # Sending email
         for chunk_customers_item in chunk_customers_data:
             email_tasks = [
                 send_welcome_email_task.s(mail_to=customer.email, mail_data=mail_data)
